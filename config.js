ffb4a6e4f64a7fc054ac5473853aa2bad502c957
qbanito <63931177+qbanito@users.noreply.github.com>
2026-08-02 17:02:27 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,1 @@
-window.TRADEBOTHUB_CONFIG = {
-  API_BASE_URL: "https://tradebothub-mev-live-api.onrender.com",
-  DEMO_FALLBACK: true,
-  REFRESH_MS: 15000
-};
+window.TRADEBOTHUB_CONFIG={API_BASE_URL:'https://tradebothub-mev-live-api.onrender.com'};
